7bbee39608033f36ffe6655a25cfd64c1766650b
Connor Horman <chorman64@gmail.com>
2025-09-12 23:56:58 -0400

    Find base address using _DYNAMIC section

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,13 @@
     macro_metavar_expr,
     slice_from_ptr_range,
     str_from_raw_parts,
-    non_null_from_ref,
     core_intrinsics // Remove
 )]
 #![cfg_attr(feature = "alloc", feature(allocator_api))]
+
+use core::ffi::c_void;
+
+use crate::elf::ElfDyn;
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
@@ -29,3 +32,18 @@ pub mod loader;
 pub mod resolver;
 
 pub mod arch;
+
+unsafe extern "C" {
+    unsafe static mut _GLOBAL_OFFSET_TABLE_: [*mut c_void; 3];
+    safe static _DYNAMIC: [ElfDyn; 4];
+}
+
+hidden_syms!(_GLOBAL_OFFSET_TABLE_, _DYNAMIC);
+
+/// Gets the load address of the dynamic linker. Useful for
+pub fn load_addr() -> *mut c_void {
+    let dyn_offset = unsafe { safe_addr_of!(_GLOBAL_OFFSET_TABLE_).cast::<usize>().read() };
+    let dyn_addr = safe_addr_of!(_DYNAMIC).cast::<c_void>().cast_mut();
+
+    dyn_addr.wrapping_byte_sub(dyn_offset)
+}
