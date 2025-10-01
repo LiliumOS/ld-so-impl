7602674b59927fea7cbf6df81c481872395f8367
Connor Horman <chorman64@gmail.com>
2025-10-01 13:16:18 -0400

    Add `dynamic_section` function

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 
 use core::ffi::c_void;
 
-use crate::elf::ElfDyn;
+use crate::elf::{DynEntryType, ElfDyn};
 
 #[cfg(feature = "alloc")]
 extern crate alloc;
@@ -43,10 +43,22 @@ unsafe extern "C" {
 
 hidden_syms!(_GLOBAL_OFFSET_TABLE_, _DYNAMIC);
 
-/// Gets the load address of the dynamic linker. Useful for
+/// Gets the load address of the current module (usually the dynamic linker).
+#[inline]
 pub fn load_addr() -> *mut c_void {
     let dyn_offset = unsafe { safe_addr_of!(_GLOBAL_OFFSET_TABLE_).cast::<usize>().read() };
     let dyn_addr = safe_addr_of!(_DYNAMIC).cast::<c_void>().cast_mut();
 
     dyn_addr.wrapping_byte_sub(dyn_offset)
+}
+
+// Helper function for obtaining the entire dynamic section of the current module (usually the dynamic linker).
+#[inline]
+pub fn dynamic_section() -> &'static [ElfDyn] {
+    let begin = safe_addr_of!(_DYNAMIC).cast::<ElfDyn>();
+    let mut end = begin;
+    while unsafe { (*end).d_tag } != DynEntryType::DT_NULL {
+        end = unsafe { end.add(1) };
+    }
+    unsafe { core::slice::from_ptr_range(begin..end) }
 }
